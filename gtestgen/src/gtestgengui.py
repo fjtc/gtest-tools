10e420388eada610296b1ba7bc35fa8f91998226
Fabio Jun Takada Chino <fjtc@users.noreply.github.com>
2015-03-20 08:07:26 -0300

    Basic user interface completed.

--- gtestgen/src/gtestgengui.py
+++ gtestgen/src/gtestgengui.py
@@ -1,5 +1,5 @@
 from Tkinter import *
-
+from tkMessageBox import *
 
 class GTestGenGUI(Frame):
     
@@ -17,10 +17,12 @@ class GTestGenGUI(Frame):
         self._menubar.add_cascade(label="File", menu=self._file_menu)
         
         self._tools_menu = Menu(self._menubar, tearoff=0)
-        self._tools_menu.add_command(label='Generate main.cpp', command = self.quit )
+        self._tools_menu.add_command(label='Generate main.cpp', command = self._do_generate_main)
         self._menubar.add_cascade(label="Tools", menu=self._tools_menu)
         
-        self._menubar.add_command(label="About")
+        self._help_menu = Menu(self._menubar, tearoff=0)
+        self._help_menu.add_command(label='About', command = self._do_about)
+        self._menubar.add_cascade(label="Help", menu=self._help_menu)
 
         root.config(menu=self._menubar)
     
@@ -29,27 +31,45 @@ class GTestGenGUI(Frame):
         self._createMenuBar()
         
         self.grid(column=0, row=0, sticky=(N, W, E, S))
-        
+
         self._test_name_label = Label(self)
         self._test_name_label['text'] = 'Test name (must be a valid C identifier.)'
         self._test_name_label.grid(column = 0, row = 0, columnspan=3, sticky=(E,W))
         
-        self._test_name = Entry(self)
-        self._test_name.grid(column = 0, row = 1, columnspan=3, sticky=(E,W))
+        self._test_name_entry = Entry(self)
+        self._test_name_entry.grid(column = 0, row = 1, columnspan=3, sticky=(E,W))
         
         self._ok_button = Button(self)
         self._ok_button['text'] = 'OK'
+        self._ok_button['command'] = self.do_generate_test
         self._ok_button.grid(column = 1, row = 2, sticky=(E,W))
         
         self._cancel_button = Button(self)
         self._cancel_button['text'] = 'Cancel'
         self._cancel_button['command'] = self.quit
         self._cancel_button.grid(column = 2, row = 2, sticky=(E,W))
+        
+        self._test_status_label = Label(self)
+        self._test_status_label['text'] = 'Test name (must be a valid C identifier.)'
+        self._test_status_label.grid(column = 0, row = 3, columnspan=3, sticky=(E,W))
+        
+    def _do_generate_main(self):
+        pass
+    
+    def do_generate_test(self):
+        pass
+
+    def _do_about(self):
+        showinfo('OK', ('gtestgen 0.1\n' + 
+                        'Copyright (c) 2015 FJTC. All rights reserved.\n' +
+                        'This software is licensed under Modified BSD License'))
+        pass
 
 if __name__ == '__main__':
     root = Tk()
+    root.wm_title('gtestgen')
     app = GTestGenGUI(master=root)
     app.mainloop()
-    root.destroy()
+    #root.destroy()
         
     
